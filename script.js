33d8a97e9af9bac0e00b83ba9db60427d2622912
NastiaKoval <anasteziyam@gmail.com>
2020-03-15 18:12:40 +0200

    feat: make slider

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const VERT_BTN = document.getElementById('vert-button');
 const VERT_SCR = document.getElementById('vert-screen');
 const HOR_BTN = document.getElementById('hor-button');
 const HOR_SCR = document.getElementById('hor-screen');
+const SLIDER = document.getElementById('slider');
+const LEFT_ARROW = document.getElementById('arrow-left');
+const RIGHT_ARROW = document.getElementById('arrow-right');
 
 MENU.addEventListener('click', (event) => {
   MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
@@ -15,4 +18,27 @@ VERT_BTN.addEventListener('click', (event) => {
 
 HOR_BTN.addEventListener('click', (event) => {
   HOR_SCR.classList.toggle('invisible');
+});
+
+let currentSlideNumber = 0;
+let slides = SLIDER.querySelectorAll('.slide');
+
+LEFT_ARROW.addEventListener('click', (event) => {
+  slides[currentSlideNumber].classList.remove('current');
+  if (currentSlideNumber == 0) {
+    currentSlideNumber = slides.length - 1;
+  } else if (currentSlideNumber > 0) {
+    currentSlideNumber -= 1;
+  }
+  slides[currentSlideNumber].classList.add('current');
+});
+
+RIGHT_ARROW.addEventListener('click', (event) => {
+  slides[currentSlideNumber].classList.remove('current');
+  if (currentSlideNumber == slides.length - 1) {
+    currentSlideNumber = 0;
+  } else if (currentSlideNumber < slides.length) {
+    currentSlideNumber = currentSlideNumber + 1;
+  }
+  slides[currentSlideNumber].classList.add('current');
 });
